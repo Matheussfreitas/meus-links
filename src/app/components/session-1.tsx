ac01528dbfcaba70612d0e7dbd80cfee573905b8
Matheus Freitas <mgsoutodelira@gmail.com>
2025-02-21 18:59:04 -0300

    update

---
+++ src/app/components/session-1.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+export default function SessionOne() {
+  return(
+    <Box sx={{
+      display: "flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: "100vh",
+      backgroundColor: "#F2F2F2",
+    }}>
+      <h1>Sessão 1</h1>
+    </Box>
+  )
+}
